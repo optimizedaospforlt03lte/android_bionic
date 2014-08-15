502294a4ed33a28c9ff01fb4a94b6dda74ad03c5
Xin Qi <xqi@codeaurora.org>
2014-08-15 18:12:32 +0000

    libc: krait: Implement optimized memmove
    * Below are the results of bionic's memmove benchmarks before and
      after this commit (on the HTC One M8 with the MSM8974 SoC).
    
    Before
    BM_string_memmove/8               100000000         12   657.29 MiB/s
    BM_string_memmove/64              100000000         17  3667.40 MiB/s
    BM_string_memmove/512              50000000         37 13729.53 MiB/s
    BM_string_memmove/1K               20000000         99 10300.25 MiB/s
    BM_string_memmove/8K                5000000        409 20021.83 MiB/s
    BM_string_memmove/16K               2000000        923 17739.73 MiB/s
    BM_string_memmove/32K                500000       3156 10379.72 MiB/s
    BM_string_memmove/64K                500000       6252 10481.72 MiB/s
    
    After
    BM_string_memmove/8               100000000         14   560.43 MiB/s
    BM_string_memmove/64              100000000         11  5782.22 MiB/s
    BM_string_memmove/512             100000000         26 18984.04 MiB/s
    BM_string_memmove/1K               50000000         56 18105.63 MiB/s
    BM_string_memmove/8K                5000000        471 17384.01 MiB/s
    BM_string_memmove/16K               2000000        973 16829.21 MiB/s
    BM_string_memmove/32K               1000000       2429 13489.39 MiB/s
    BM_string_memmove/64K                500000       4726 13866.15 MiB/s
    
    Change-Id: I227873f4b4491ab201592a8d240d4a0e818e737f
    
    # Conflicts:
    #	libc/arch-arm/krait/krait.mk

--- libc/arch-arm/krait/krait.mk
+++ libc/arch-arm/krait/krait.mk
@@ -15,17 +15,16 @@ libc_bionic_src_files_exclude_arm += \
 
 libc_bionic_src_files_arm += \
     arch-arm/krait/bionic/memcpy.S \
+    arch-arm/krait/bionic/memmove.S \
     arch-arm/krait/bionic/memset.S \
     arch-arm/krait/bionic/strcmp.S \
     arch-arm/krait/bionic/__strcat_chk.S \
     arch-arm/krait/bionic/__strcpy_chk.S \
 
-# Use cortex-a15 versions of strcat/strcpy/strlen and standard memmove
+# Use cortex-a15 versions of strcat/strcpy/strlen
 libc_bionic_src_files_arm += \
     arch-arm/cortex-a15/bionic/stpcpy.S \
     arch-arm/cortex-a15/bionic/strcat.S \
     arch-arm/cortex-a15/bionic/strcpy.S \
     arch-arm/cortex-a15/bionic/strlen.S \
 
-libc_bionic_src_files_arm += \
-    arch-arm/denver/bionic/memmove.S \
